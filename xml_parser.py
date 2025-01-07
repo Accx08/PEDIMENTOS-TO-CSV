a917088f9d208114db1059b798c09625383c15cd
Accx08 <achilpa0806@gmail.com>
2025-01-06 18:18:05 -0600

    Updated fields in products

--- xml_parser.py
+++ xml_parser.py
@@ -1,9 +1,19 @@
 import xml.etree.ElementTree as ET
+from datetime import datetime
 
 def parse_xml(file_path):
     tree = ET.parse(file_path)
     root = tree.getroot()
     data = {"general": [], "productos": []}
+
+    def format_date(date_str):
+        if date_str:
+            try:
+                date_obj = datetime.strptime(date_str, '%Y%m%d')
+                return date_obj.strftime('%d/%m/%Y')
+            except:
+                return date_str
+        return ''
 
     for at001_row in root.findall(".//at001/row"):
         def get_float(element, field):
@@ -12,39 +22,48 @@ def parse_xml(file_path):
 
         tipo_cambio = get_float(at001_row, "F001TIPCAM")
         general_data = {
-            "Pedimento": at001_row.findtext("C001NUMPED"),
-            "Aduana": at001_row.findtext("C001ADUSEC"),
-            "CVE.Pedimento": at001_row.findtext("C001CVEDOC"),
-            "T.Operacion": at001_row.findtext("C001TIPREG"),
-            "TC": tipo_cambio,
-            "valor_dolares": at001_row.findtext("F001VALDOL"),
-            "valor_aduana": at001_row.findtext("N001VALADU"),
+            "Pedimento"         : at001_row.findtext("C001NUMPED"),
+            "empresa"           : at001_row.findtext("C001NOMCLI"),
+            "Aduana"            : at001_row.findtext("C001ADUSEC"),
+            "CVE.Pedimento"     : at001_row.findtext("C001CVEDOC"),
+            "T.Operacion"       : at001_row.findtext("C001TIPREG"),
+            "Fecha"             : format_date(at001_row.findtext("D001FECEP")),
+            "TC"                : tipo_cambio,
+            "valor_dolares"     : at001_row.findtext("F001VALDOL"),
+            "valor_aduana"      : at001_row.findtext("N001VALADU"),
             "zcnPrecioComercial": at001_row.findtext("N001VALCOM"),
-            "Patente": at001_row.findtext("C001PATEN"),
-            "val_seguro": get_float(at001_row, "F001VALSEG") * tipo_cambio,
-            "seguro": get_float(at001_row, "F001SEGURO") * tipo_cambio,
-            "Flete aerero": get_float(at001_row, "F001FLETES") * tipo_cambio,
-            "embalaje": get_float(at001_row, "F001EMBALA") * tipo_cambio,
-            "O.Incrementales": get_float(at001_row, "F001OTRINC") * tipo_cambio,
-            "PRV": 290,
-            "PRV iva": 290*0.16,
+            "Patente"           : at001_row.findtext("C001PATEN"),
+            "val_seguro"        : get_float(at001_row, "F001VALSEG") * tipo_cambio,
+            "seguro"            : get_float(at001_row, "F001SEGURO") * tipo_cambio,
+            "Flete aerero"      : get_float(at001_row, "F001FLETES") * tipo_cambio,
+            "embalaje"          : get_float(at001_row, "F001EMBALA") * tipo_cambio,
+            "O.Incrementales"   : get_float(at001_row, "F001OTRINC") * tipo_cambio,
+            "PRV"               : at001_row.findtext("F001TASPRE"),
+            "PRV iva"           : get_float(at001_row, "F001OTRINC") *0.16,
+            "DTA"               : get_float(at001_row, "I001TTDTA1"),
         }
+        
         data["general"].append(general_data)
 
     for at016_row in root.findall(".//at016/row"):
+        at019_provider = root.findall(".//at019/row")
+        provider = at019_provider.findtext("C019IDE2CAS")
         product = {
             "Pedimento": at016_row.findtext("C016NUMPED"),
-            "descripcion": at016_row.findtext("C016DESMER"),
-            "fraccion": at016_row.findtext("C016FRAC"),
-            "subfraccion": at016_row.findtext("I016SUBFRA"),
-            "cantidad_umc": at016_row.findtext("F016CANUMC"),
-            "unidad_medida_umc": at016_row.findtext("C016UNIUMC"),
-            "cantidad_umt": at016_row.findtext("F016CANUMT"),
-            "unidad_medida_umt": at016_row.findtext("C016UNIUMT"),
-            "valor_dolares": at016_row.findtext("F016VALDOL"),
-            "precio_unitario": at016_row.findtext("F016PREUNI"),
-            "pais_origen": at016_row.findtext("C016PAISOD"),
-        }
+            "descripcion"       : at016_row.findtext("C016DESMER"),
+            "fraccion"          : at016_row.findtext("C016FRAC"),
+            "subfraccion"       : at016_row.findtext("I016SUBFRA"),
+            "cantidad_umc"      : get_float(at016_row, "F016CANUMC"),
+            "unidad_medida_umc" : at016_row.findtext("C016UNIUMC"),
+            "cantidad_umt"      : get_float(at016_row, "F016CANUMT"),
+            "unidad_medida_umt" : at016_row.findtext("C016UNIUMT"),
+            "valor_dolares"     : get_float(at016_row, "F016VALDOL"),
+            "precio_unitario"   : get_float(at016_row, "F016PREUNI"),
+            "valor_aduana"      : get_float(at016_row, "N016VALADU"),
+            "iva_producto"      : get_float(at016_row, "N016VALADU") * get_float(at016_row, "F016TASIVA")/100,
+            "igi"               : get_float(at016_row, "N016MONIGI"),
+            "prov"              : provider,
+       }
         data["productos"].append(product)
 
     return data
